4f10f4f0973f7bc4a8fb2ff4e3f4700580d41b70
Walisson Rodrigo <walissonrodrigo@outlook.com>
2020-07-13 19:57:24 -0300

    Adicionado Proptypes nos componentes.

--- src/components/ProjectItem/ProjectItem.jsx
+++ src/components/ProjectItem/ProjectItem.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import PropTypes from "prop-types";
 import IconButton from "@material-ui/core/IconButton";
 import DeleteIcon from "@material-ui/icons/Delete";
 import EditIcon from "@material-ui/icons/Edit";
@@ -85,4 +86,26 @@ const ProjectItem = ({
     </List>
   );
 };
+
+ProjectItem.defaultProps = {
+  project: {
+    title: "",
+    dateStart: moment().format("YYYY-MM-DD[T]HH:mm:ss"),
+    dateEnd: moment().format("YYYY-MM-DD[T]HH:mm:ss"),
+  },
+  handleCreateTask: function () {},
+  handleUpdateProject: function () {},
+  handleDeleteProject: function () {},
+};
+
+ProjectItem.propTypes = {
+  // Properties
+  project: PropTypes.object.isRequired,
+
+  // Events
+  handleCreateTask: PropTypes.func.isRequired,
+  handleUpdateProject: PropTypes.func.isRequired,
+  handleDeleteProject: PropTypes.func.isRequired,
+};
+
 export default ProjectItem;
